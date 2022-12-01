285d3c3ed9c7ede796e20087dda39a3116313987
林晓丽 <9666593+lin-xiao-li@user.noreply.gitee.com>
2022-12-01 13:23:27 +0800

    登录

--- MedicineMall/mall-auth-server/src/main/java/com/example/auth/feign/MemberFeignService.java
+++ MedicineMall/mall-auth-server/src/main/java/com/example/auth/feign/MemberFeignService.java
@@ -1,15 +1,18 @@
 package com.example.auth.feign;
 
+import com.example.auth.vo.UserLoginVo;
 import com.example.auth.vo.UserRegistVo;
 import com.example.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("mall-member")
+@FeignClient("mall-user")
 public interface MemberFeignService {
-    @PostMapping(value = "/member/member/register")
+    @PostMapping(value = "/user/user/register")
     R register(@RequestBody UserRegistVo vo);
 
+    @PostMapping(value = "/user/user/login")
+    R login(@RequestBody UserLoginVo vo);
 
 }
